6ad530625581fe27dd97928e30e1438d0c8536e4
Minh Le <beaver700nh@gmail.com>
2023-03-14 21:56:54 -0400

    Revert "Adding to GitHub"
    This reverts commit 741c50b1f3a2af8b612c33b7c127a7939de0983c.

--- build/CMakeFiles/ed65.dir/src/common.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ed65.dir/src/common.cpp.o: \
- /home/alarm/Programming/ed65/src/common.cpp /usr/include/stdc-predef.h \
- /home/alarm/Programming/ed65/src/common.hpp
--- src/main.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_HPP
-#define MAIN_HPP
-
-void startup(int *rows, int *cols);
-void cleanup(int signal);
-
-#endif
